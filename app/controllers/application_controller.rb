329b15877259cac97e121866d707ab40f11e5d28
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-18 12:22:52 -0800

    remove broken helper from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
-  helper_method :login_user!(user)
-  
+
   private
   def login_user!(user)
     session[:session_token] = user.reset_session_token!
