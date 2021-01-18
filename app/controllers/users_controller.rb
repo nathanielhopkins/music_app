6163dd6532635abf436c694502fcd39be8bd2b3a
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-18 11:16:15 -0800

    write methods in UsersController to allow creation and login of new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
 
-    
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.login!
+      render json: "Welcome to the site, #{@user.email}"
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
