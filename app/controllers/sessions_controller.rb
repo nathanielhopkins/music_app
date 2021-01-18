3e1b25cd1079efad05ac79621c7f4c693c7c8b11
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-18 11:57:53 -0800

    write methods for SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+    render :new
+  end
 
+  def create
+    @user = User.find_by_credentials(
+      params[user][email],
+      params[user][password]
+    )
+    login_user!(@user)
+    redirect_to user_url(@user)
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
 end
