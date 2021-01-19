664de0bab06dde844863a412dd80d9915b3933d9
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-18 21:56:53 -0800

    refactor logout logic from application_controller into sesssions_controller; dry up code in application_controller;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
-  helper_method :logged_in?
+  helper_method :current_user, :logged_in?
 
   private
   def login_user!(user)
     session[:session_token] = user.reset_session_token!
   end
 
-  def logout!
-    current_user.reset_session_token!
-    session[:session_token] = nil
-  end
-
   def logged_in?
-    !current_user.nil?
+    !!current_user
   end
 
   def current_user
