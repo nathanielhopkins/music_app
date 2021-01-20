06ab4e52c8c223596971674efed58277fea604d4
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-20 15:11:59 -0800

    remove :index from :albums routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :bands do
     resources :albums, only: [:new]
   end
-  resources :albums, except: [:new]
+  resources :albums, except: [:new, :index]
 end
