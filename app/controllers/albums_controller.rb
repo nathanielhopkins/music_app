9748cc890ce731b7079ab3747cc655424e6efb37
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-21 14:13:12 -0800

    fix query issue in #destory in AlbumsController

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -41,7 +41,7 @@ class AlbumsController < ApplicationController
   end
   
   def destroy
-    @album = Album.find_by(params[:id])
+    @album = Album.find_by(id: params[:id])
     @album.delete
     redirect_to band_url(@album.band_id)
   end
