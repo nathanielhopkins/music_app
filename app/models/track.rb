d820887159eac796c10c7ea98d2b966b17ff3fad
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-22 11:39:46 -0800

    create Track model, write validations and associations to band and album

---
+++ app/models/track.rb
@@ -0,0 +1,19 @@
+class Track < ApplicationRecord
+  validates :album_id, :title, :ord, presence: true
+  validates :bonus, inclusion: { in: [true, false]}
+  validates :ord, uniqueness: { scope: :album_id }
+
+  belongs_to(
+    :album,
+    class_name: "Album",
+    foreign_key: :album_id,
+    primary_key: :id
+  )
+
+  belongs_to(
+    :band,
+    through: :album,
+    source: :band
+  )
+
+end
