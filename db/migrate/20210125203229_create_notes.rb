33a1b3eb775fcaebdef5446ae2425353c5cf6c2f
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-25 12:43:08 -0800

    create and migrate CreateNotes migration

---
+++ db/migrate/20210125203229_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notes do |t|
+      t.integer :track_id, null: false
+      t.integer :user_id, null: false
+      t.text :content, null: false
+    end
+
+    add_index :notes, :track_id
+    add_index :notes, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_192048) do
+ActiveRecord::Schema.define(version: 2021_01_25_203229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2021_01_22_192048) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_bands_on_name"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.integer "track_id", null: false
+    t.integer "user_id", null: false
+    t.text "content", null: false
+    t.index ["track_id"], name: "index_notes_on_track_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "tracks", force: :cascade do |t|
