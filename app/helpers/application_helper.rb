6d99d448cc3390c938b01c5e11d1f7a5eb63f9b1
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-26 13:15:27 -0800

    write helper method #ugly_lyrics to format track.lyrics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     html += 'value="<%= form_authenticity_token %>">'
     html.html_safe
   end
+
+  def ugly_lyrics(lyrics)
+    processed_lyrics = ""
+    lyrics.lines.each do |line|
+      processed_lyrics << "&#9835; #{h(line)}"
+    end
+    "<pre class='lyrics'>#{processed_lyrics}</pre>".html_safe   
+  end
 end
